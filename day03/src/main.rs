ab6b7807766d46f9eeb78a1e5676b64fbfe6a0e6
Patrik Görling <p.gorling@gmail.com>
2022-12-21 19:20:08 +0100

    started day3

---
+++ day03/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+fn part1() {
+    let content = fs::read_to_string("input.txt").expect("Expected to read file");
+}
+
+fn part2() {
+    let content = fs::read_to_string("input.txt").expect("Expected to read file");
+}
+
+fn main() {
+    part1();
+    part2();
+}
