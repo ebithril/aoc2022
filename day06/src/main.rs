6539e3baf85e7e1381515711789c594b47bfc7ad
Patrik Görling <p.gorling@gmail.com>
2022-12-30 10:53:16 +0100

    day6 part1

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,7 +1,27 @@
 use std::fs;
+use std::collections::VecDeque;
 
 fn part1() {
     let content = fs::read_to_string("input.txt").expect("Expected to read file");
+    let mut sequence: VecDeque<char> = VecDeque::new();
+
+    for (i, c) in content.chars().enumerate() {
+        for (j, oc) in sequence.iter().enumerate() {
+            if *oc == c {
+                for _ in 0..j+1 {
+                    sequence.pop_front();
+                }
+                break;
+            }
+        }
+
+        sequence.push_back(c);
+
+        if sequence.len() == 4 {
+            println!("Part1: {}", i + 1);
+            return;
+        }
+    }
 }
 
 fn part2() {
